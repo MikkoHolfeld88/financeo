af03e22467b8719687317d0e61768ec7883efdbe
jason.roar@web.de <Bitch-11>
2022-07-03 16:01:43 +0200

    add: firebase functionality

--- financeo-frontend/src/App.tsx
+++ financeo-frontend/src/App.tsx
@@ -2,24 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { initializeApp } from 'firebase/app';
-
-// TODO: Replace the following with your app's Firebase project configuration
-const firebaseConfig = {
-
-};
-
-const app = initializeApp(firebaseConfig);
-
-const test = () => {
-  console.log(process.env.TEST)
-}
-
 function App() {
-  test()
   return (
     <div className="App">
-
+      <h1>TEST</h1>
     </div>
   );
 }
