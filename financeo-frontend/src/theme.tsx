b7844492453e29c527eb2d5ef9a17e28f82c221d
jason.roar@web.de <Bitch-11>
2022-07-25 21:39:06 +0200

    modify: theme and color structure

--- financeo-frontend/src/theme.tsx
+++ financeo-frontend/src/theme.tsx
@@ -1,18 +1,27 @@
-import { createTheme } from '@mui/material/styles';
-import { green } from '@mui/material/colors';
+import {createTheme} from '@mui/material/styles';
 import * as COLORS from './constants/colors';
 
 const theme = createTheme({
     palette: {
         primary: {
             main: COLORS.SCHEME.mainBackground,
+            // light: '',
+            // dark: '',
+            // contrastText: ''
         },
         secondary: {
-            main: green[500],
+            main: COLORS.SCHEME.background,
+        },
+        error: {
+            main: COLORS.SCHEME.error,
         },
         warning: {
             main: COLORS.SCHEME.warn,
-        }
+        },
+        // info: {
+        // },
+        // success: {
+        // }
     },
 });
 
