55f3db1ecc4cec18ade550c830b81080e204e67b
jason.roar@web.de <Bitch-11>
2022-08-13 23:04:34 +0200

    change: .eslintrc.js, firestore.rules

--- financeo-frontend/functions/.eslintrc.js
+++ financeo-frontend/functions/.eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-  root: "src",
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
-  rules: {
-    // quotes: ["error", "double"],
-  },
+  // root: "src",
+  // env: {
+  //   es6: true,
+  //   node: true,
+  // },
+  // extends: [
+  //   "eslint:recommended",
+  //   "google",
+  // ],
+  // rules: {
+  //   // quotes: ["error", "double"],
+  // },
 };
