d980d5ab901ea3206c04c34129fe6cfb1bf4754a
Mikko Holfeld <Bitch-11>
2022-12-31 09:17:52 +0100

    add: Matcher to CategoryCreation.tsx, refactor: Submenu.tsx

--- financeo-frontend/src/components/account/categories/CategoryCreation.tsx
+++ financeo-frontend/src/components/account/categories/CategoryCreation.tsx
@@ -1,7 +1,11 @@
-import {Container, Grid, TextField, Typography} from "@mui/material";
+import {Container, Grid, TextField, Tooltip, Typography} from "@mui/material";
 import React, {useState} from "react";
 import Button from "@mui/material/Button";
 import {CategoryIconPicker} from "./CategoryIconPicker";
+import AddIcon from "@mui/icons-material/Add";
+import IconButton from "@mui/material/IconButton";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import * as COLORS from "../../../constants/colors";
 
 const HEADLINE = "Create category";
 const EXPLANATION = "Enter category name, icon and matchers to provide meaningful " +
@@ -9,7 +13,9 @@ const EXPLANATION = "Enter category name, icon and matchers to provide meaningfu
     "Nevertheless individualizing category labels helps investigating your personal financial behaviour.";
 
 export const CategoryCreation = () => {
+    let matchers: string[] = [];
     const [name, setName] = useState("");
+    const [matcher, setMatcher] = useState("");
     const [description, setDescription] = useState("");
     const [value, setValue] = useState<null | string>(null);
     const [iconDialogOpen, setIconDialog] = React.useState(false);
@@ -17,7 +23,16 @@ export const CategoryCreation = () => {
     const pickIcon = (icon: string | null) => {
         setIconDialog(false);
         setValue(icon);
-        console.log(icon);
+    };
+
+    const addMatcher = () => {
+        matchers.push(matcher);
+        setMatcher("");
+    };
+
+    const removeMatcher = () => {
+        matchers = matchers.filter(matcherToRemove => matcherToRemove !== matcher);
+        setMatcher("");
     };
 
     return (
@@ -47,6 +62,31 @@ export const CategoryCreation = () => {
                             helperText="Enter category description"
                             onChange={(event) => setDescription(event.target.value)}
                         />
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={10} lg={10} xl={10} className="textFieldGrid">
+                        <TextField
+                            variant="standard"
+                            fullWidth
+                            label="Matcher"
+                            value={description}
+                            style={{fontSize: "20px"}}
+                            helperText="Enter category description"
+                            onChange={(event) => setMatcher(event.target.value)}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={1} lg={1} xl={1} className="textFieldGrid">
+                        <Tooltip title="Add matcher to matchers list" followCursor>
+                            <IconButton onClick={() => addMatcher()} aria-label="delete matcher">
+                                <AddIcon color="primary"/>
+                            </IconButton>
+                        </Tooltip>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={1} lg={1} xl={1} className="textFieldGrid">
+                        <Tooltip title="Remove matcher to matchers list" followCursor>
+                            <IconButton onClick={() => removeMatcher()} aria-label="delete matcher">
+                                <DeleteForeverIcon sx={{color: COLORS.SCHEME.warn}}/>
+                            </IconButton>
+                        </Tooltip>
                     </Grid>
                     <Grid item xs={12} sm={12} md={12} lg={12} xl={12} className="iconOfList">
                         <CategoryIconPicker value={value} setValue={setValue} pickIcon={pickIcon} open={iconDialogOpen} setOpen={setIconDialog}/>
